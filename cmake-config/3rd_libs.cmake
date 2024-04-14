8c5206f2e169fd124ca2923ec419a08372aae525
ourenjiang <orejia@163.com>
2024-04-14 22:04:25 +0800

    添加官方示例。

--- cmake-config/3rd_libs.cmake
+++ cmake-config/3rd_libs.cmake
@@ -2,7 +2,7 @@
 # 第1类：仅头文件库、以静态库方式引入的库、测试库，都属于开发依赖
 # 第2类：以动态库的方式参与开发和生产环境的运行；
 
-# # boost-1.81.0
-# set(BOOST_PATH $ENV{HOME}/devtool/boost-1.81.0)
-# include_directories(${BOOST_PATH}/include)
-# link_directories(${BOOST_PATH}/lib)
+# benchmark-1.8.3
+set(BENCHMARK_PATH $ENV{HOME}/devtool/benchmark-1.8.3)
+include_directories(${BENCHMARK_PATH}/include)
+link_directories(${BENCHMARK_PATH}/lib)
