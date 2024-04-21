342e9fceb643681e687b32a00432d3072d754f98
ourenjiang <orejia@163.com>
2024-04-21 22:17:22 +0800

    first commit

--- cmake-config/compiler_cfg.cmake
+++ cmake-config/compiler_cfg.cmake
@@ -12,3 +12,23 @@ link_directories(${CMAKE_BINARY_DIR}/lib)
 set(CMAKE_CXX_FLAGS
 -Wall
 )
+
+# set(CMAKE_CXX_CLANG_TIDY
+#     "clang-tidy;--enable-check-profile;--checks=-*,
+#     abseil-string-find-startswith,bugprone-*,cert-*,
+#     clang-analyzer-*,cppcoreguidelines-*,google-*,hicpp-*,
+#     llvm-*,misc-*,modernize-*,-modernize-use-trailing-return-type,
+#     performance-*,readability-*,-readability-static-definition-in-anonymous-namespace,
+#     -readability-simplify-boolean-expr,portability-*")
+
+# # 可用格式1
+# set(CMAKE_CXX_CLANG_TIDY "clang-tidy;\
+#     --enable-check-profile;\
+#     --checks=-*,google-*")
+
+# 可用格式2
+# 启用检查器配置:引用工程中.clang-tidy配置中的配置项。实测中，我将该配置文件放在源码根目录下。
+# set(CMAKE_CXX_CLANG_TIDY "clang-tidy; --enable-check-profile;")
+# set(CMAKE_CXX_CLANG_TIDY "clang-tidy; -check=*;")
+set(CMAKE_CXX_CLANG_TIDY clang-tidy;
+    -config-file=${PROJECT_SOURCE_DIR}/.clang-tidy;)
---
+++ empty_project/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <map>
+using namespace std;
+int main()
+{
+    string hello{ "world" };
+    // string() = "hello";
+    char* ptr = nullptr;
+    // *ptr = 'h';
+    int val2;
+
+}
